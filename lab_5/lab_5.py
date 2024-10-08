a9f4dd60b79b70648f5ded987da9a0f5b2378f31
Ginky <krosh024@gmail.com>
2024-10-08 08:17:51 +0300

    add lab_5 py

---
+++ lab_5/lab_5.py
@@ -0,0 +1,23 @@
+def insert_text_at_line(file_path, text, line_number):
+
+    if line_number < 1:
+        raise ValueError("Номер строки должен быть положительным целым числом.")
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    if line_number > len(lines) + 1:
+        raise ValueError("Номер строки превышает количество строк в файле.")
+
+    lines.insert(line_number - 1, text + '\n')
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.writelines(lines)
+
+
+# Пример использования
+file_path = 'example.txt'
+text_to_insert = 'привет'
+line_to_insert = 3
+
+insert_text_at_line(file_path, text_to_insert, line_to_insert)
